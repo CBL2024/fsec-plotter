cc1adc0818e1fabce4b3f941cfff517db50b5b6e
Claudia Burn-Leefe <claudia.burn-leefe@diamond.ac.uk>
2024-08-21 16:27:37 +0100

    update with baseline correction and supervised model

--- build/lib/fsec_extractor/fsec_script.py
+++ build/lib/fsec_extractor/fsec_script.py
@@ -9,6 +9,11 @@ import re
 from collections import defaultdict
 import tkinter as tk
 from tkinter import *
+import BaselineRemoval
+from BaselineRemoval import BaselineRemoval
+import tensorflow as tf
+from keras.preprocessing import image
+import numpy as np
 
 
 def run():
@@ -25,15 +30,18 @@ def run():
         def extract_retention_time():
 
             def check_flow_rate(method):
-                if (method.startswith(
-                        'C:\LabSolutions\Data\Project1\Methods\GFP+TRYP\Microtitre plate autosampler\Fast 1.5ml_min')
-                        or method.startswith('C:\\LabSolutions\\Data\\Project1\\Methods\\GFP+TRYP\\FAST 1.5 ML_MIN')):
+                if "1.5 ML_MIN" in method_file:
                     flow_rate = 1.5
-                elif method.startswith(
-                        'C:\LabSolutions\Data\Project1\Methods\GFP+TRYP\Microtitre plate autosampler\Fast 1 ml_min'):
-                    flow_rate = 1.0
+                elif "1.5ml_min" in method_file:
+                    flow_rate = 1.5
+                elif "1-5ml flow" in method_file:
+                    flow_rate = 1.5
+                elif '1ML_MIN' in method_file:
+                    flow_rate = 1
+                elif '1 ml_min' in method_file:
+                    flow_rate = 1
                 else:
-                    flow_rate = 1.0
+                    flow_rate = 0.5
                 return flow_rate
 
             start_index = None
@@ -144,11 +152,25 @@ def run():
                                                         col != 'Retention Volume (mL)']
             measure_table = combined_df[column_order]
 
+            def correct_baselines(measure_table):
+                '''Uses BaselineRemoval package Zhangfit method to correct baselines.'''
+                for col in measure_table.columns:
+                    try:
+                        if col != 'Retention Volume (mL)':
+                            baseObj = BaselineRemoval(measure_table[col])
+                            Zhangfit_output = baseObj.ZhangFit()
+                            measure_table[col] = Zhangfit_output
+                    except ValueError or IndexError:
+                        open_error_window("Baseline correction failed.")
+
+                return measure_table
+
             # Save the combined DataFrame to a CSV file
             if condense_factor == 12:
-                measure_table.to_csv(os.path.join(path, str(measure) + ".csv"), index=False)
+                correct_baselines(measure_table).to_csv(os.path.join(path, str(measure) + ".csv"), index=False)
 
-            return measure_table
+            return correct_baselines(measure_table)
+
 
         # Plot graphs
 
@@ -233,62 +255,141 @@ def run():
         if condense_factor == 1:
             check_detergent_names(combine_dataframes(extract_retention_time(), extract_intensities(), 1.5))
 
+    
+##  M A C H I N E L E A R N I N G
+    def graphs_ml(path):
+        end_marker = {"GFP_fluorescence": "[LC Chromatogram(Detector B-Ch2)]"}
+        files = glob.glob(os.path.join(path, "*.txt"))
+        df = pd.DataFrame()
+        output_dir = os.path.join(path,'tmp')
+        if not os.path.exists(output_dir):
+            os.mkdir(os.path.join(path,'tmp'))
+        for file in files:
+            for e in end_marker:
+                try:
+                    with open(file) as f:
+                        lines = f.readlines()
+                        start_index = None
+                        end_index = None
+                        for i, line in enumerate(lines):
+                            if 'R.Time (min)\tIntensity' in line:
+                                start_index = i + 1
+                            elif 'Method File' in line:
+                                method_file = line.split('\t')[1].strip()
+                            elif end_marker.get(e) in line:
+                                end_index = i
+                                break
+
+                        if start_index is not None and end_index is not None:
+                            
+                            def flow_rate(method_file):
+                                if "1.5 ML_MIN" in method_file:
+                                    flow_rate = 1.5
+                                elif "1.5ml_min" in method_file:
+                                    flow_rate = 1.5
+                                elif "1-5ml flow" in method_file:
+                                    flow_rate = 1.5
+                                elif '1ML_MIN' in method_file:
+                                    flow_rate = 1
+                                elif '1 ml_min' in method_file:
+                                    flow_rate = 1
+                                else:
+                                    flow_rate = 0.5
+                                return flow_rate
+
+                            section = lines[start_index:end_index]
+                            df = pd.read_csv(StringIO(''.join(section)), sep='\t', engine='python', header=None)
+                            plt.close('all')
+                            x = df[0] * flow_rate(method_file)
+                            y = df[1]
+                            
+                            fig, ax = plt.subplots()
+                            ax.plot(x,y)                        
+                            ax.autoscale()
+                            ax.axis("off")                        
+                            ax.set_xticks([])
+                            ax.set_yticks([])
+                            ax.fill_between(x, y, where=[v > 10000 for v in y], color='r', interpolate=True) # Make high signal red. Anything below <10000 is unusable.
+                            ax.fill_between(x, y, where=[12 > z > 10.5 for z in x], color='green', interpolate=True) # At this volume free GFP elutes as a sharp peak.
+                            ax.fill_between(x, y, where=[z < 6.7 for z in x], color='yellow', interpolate=True) # 6-6.7 should colour the void peak.
+                            ax.fill_between(x, y, where=[z < 6 for z in x], color='gray', interpolate=True) # 6 is the void volume so anything before is not the sample.                                ax.fill_between(x, y, where=[v < 10000 for v in y], color='b', interpolate=True) # Make plot blue if signal is low.
+                            filename = os.path.basename(file)
+                            plot_path = os.path.join(f"{output_dir}", f"{filename}.png")
+                            plt.savefig(plot_path)
+
+                except (pd.errors.ParserError, pd.errors.EmptyDataError, ValueError) as ex:
+                    print(f"Error in {file}...Skipping... Error: {ex}")
+                    files.remove(file)
+                    break
+
+    def classify(path):
+
+        model = tf.keras.models.load_model("C:\\Users\\qdj48121\\Documents\\fsec_script\\src\\fsec_extractor\\model.keras")
+
+        model.compile(loss='categorical_crossentropy',
+                    optimizer='rmsprop',
+                    metrics=['accuracy'])
+
+        # predicting images
+        image_file_dir = os.path.join(path, 'tmp')
+        all_images = os.listdir(image_file_dir)
+        for i in all_images:
+            img = image.load_img(os.path.join(image_file_dir,i), target_size=(150, 150))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+
+            images = np.vstack([x])
+            classes = model.predict(images)
+            print(classes)
+            #os.rmtree(image_file_dir)
+
     def extractor_gui():
         """The GUI for the data extractor."""
 
-        master = Tk()  # The master window.
+        master = Tk()
         master.title('F-SEC data extractor')
         background_color = 'white'
         master.config(bg=background_color)
         master.resizable(False, False)
 
         is_detergent_screen_tk = tk.BooleanVar()  # Checkbutton for detergent screen
-        Checkbutton(master, text='12 detergent screen ', variable=is_detergent_screen_tk, bg=background_color).grid(row=4, column=1, padx=10,
-                                                                                                             sticky=W)
+        Checkbutton(master, text='12 detergent screen ', variable=is_detergent_screen_tk, bg=background_color).grid(
+            row=4, column=1, padx=10,
+            sticky=W)
         path_tk = StringVar()  # Entry box for directory
         Label(master, width=30, anchor="e", text='Path to folder containing .txt files:', fg="red",
-              font=("sans",8,"bold"), bd=5, bg=background_color).grid(row=1, padx=(10,20))
-        Entry(master, textvariable=path_tk, bg='#fe7c7c', width = 20).grid(row=1, column=1, padx=(0,10))
+              font=("sans", 8, "bold"), bd=5, bg=background_color).grid(row=1, padx=(10, 20))
+        Entry(master, textvariable=path_tk, bg='grey', width=20).grid(row=1, column=1, padx=(0, 10))
 
-        a1 = StringVar()
-        a2 = StringVar()
-        a3 = StringVar()
-        a4 = StringVar()
-        a5 = StringVar()
-        a6 = StringVar()
-        a7 = StringVar()
-        a8 = StringVar()
+        alpha = []
+        for i in range(8):
+            alpha.append(StringVar())
 
-        d1 = StringVar()
-        d2 = StringVar()
-        d3 = StringVar()
-        d4 = StringVar()
-        d5 = StringVar()
-        d6 = StringVar()
-        d7 = StringVar()
-        d8 = StringVar()
-        d9 = StringVar()
-        d10 = StringVar()
-        d11 = StringVar()
-        d12 = StringVar()
+        delta = []
+        for i in range(12):
+            delta.append(StringVar())
 
-        alpha = [a1, a2, a3, a4, a5, a6, a7, a8]  # This creates the entry fields for the proteins
-
-        Label(master, font=("arial",8), width=20, anchor="w", text='(Optional) Names of constructs ', bd=5, bg=background_color).grid(
-            row=6,padx=(10,110))
-        Label(master, font=("arial",8), width=20, anchor="w", text='for each row (A-H):', bd=5, bg=background_color).grid(
-            row=7,padx=(10,100))
+        Label(master, font=("arial", 8), width=20, anchor="w", text='(Optional) Names of constructs ', bd=5,
+              bg=background_color).grid(
+            row=6, padx=(10, 110))
+        Label(master, font=("arial", 8), width=20, anchor="w", text='for each row (A-H):', bd=5,
+              bg=background_color).grid(
+            row=7, padx=(10, 100))
         for a, i in zip(alpha, range(len(alpha))):
-            Entry(master, borderwidth=2, relief="sunken", width=20, textvariable=a, bg='white').grid(row=9+i, column=0, padx=(10,100))
+            Entry(master, borderwidth=2, relief="sunken", width=20, textvariable=a, bg='white').grid(row=9 + i,
+                                                                                                     column=0,
+                                                                                                     padx=(10, 100))
 
-        delta = [d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12]  # Entry fields for detergents
-
-        Label(master,font=("arial",8), width=30, anchor="w", text="(Optional) Names of detergents", bg=background_color).grid(row=6,
-                                                                                                             column=1,padx=(10,10))
-        Label(master,font=("arial",8), width=30, anchor="w", text=" for each column (1-12):", bg=background_color).grid(row=7,
-                                                                                                             column=1,padx=(10,10))
+        Label(master, font=("arial", 8), width=30, anchor="w", text="(Optional) Names of detergents",
+              bg=background_color).grid(row=6,
+                                        column=1, padx=(10, 10))
+        Label(master, font=("arial", 8), width=30, anchor="w", text=" for each column (1-12):",
+              bg=background_color).grid(row=7,
+                                        column=1, padx=(10, 10))
         for d, i in zip(delta, range(len(delta))):
-            Entry(master, borderwidth=2, relief="sunken",width=20, textvariable=d, bg='white').grid(row=9 + i, column=1, padx=(0,10))
+            Entry(master, borderwidth=2, relief="sunken", width=20, textvariable=d, bg='white').grid(row=9 + i,
+                                                                                                     column=1,
+                                                                                                     padx=(0, 10))
 
         end_marker = {"UV_absorbance": "[LC Chromatogram(Detector A-Ch2)]",
                       "GFP_fluorescence": "[LC Chromatogram(Detector B-Ch2)]",
@@ -296,22 +397,73 @@ def run():
 
         def submit():
             """Stores variables after pressing submit button"""
-            names = [str(a1.get()), str(a2.get()), str(a3.get()), str(a4.get()),
-                     str(a5.get()), str(a6.get()), str(a7.get()), str(a8.get())]
-            print(names)
-            detergents = list(filter(lambda item: item != "", [str(d1.get()), str(d2.get()), str(d3.get()),
-                                                               str(d4.get()), str(d5.get()), str(d6.get()),
-                                                               str(d7.get()), str(d8.get()), str(d9.get()),
-                                                               str(d10.get()),
-                                                               str(d11.get()), str(d12.get())]))
+            names = []
+            for a in range(len(alpha)):
+                names.append(str(alpha[a].get()))
+            detergents_ls = []
+            for d in range(len(delta)):
+                detergents_ls.append(str(delta[d].get()))
+            detergents = (list(filter(lambda item: item != "", detergents_ls)))
             for e in end_marker:
                 for f in [12, 1]:
                     extractor(str(path_tk.get()), int(is_detergent_screen_tk.get()), names, e,
                               end_marker.get(e), detergents, f)
+                    
+            graphs_ml(str(path_tk.get()))
+            classify(str(path_tk.get()))
             master.destroy()
 
-        Button(master, text='Submit', font=("arial",10,"bold"),command=submit, width=10,
-               borderwidth=1).grid(row=40, column=0, pady=10, padx=(100,0))
+        def plate_button():
+            master.destroy()
+            plate_gui()
+
+        Button(master, text='Submit', font=("arial", 10, "bold"), command=submit, width=10,
+               borderwidth=1).grid(row=40, column=0, pady=10, padx=(100, 0))
+
+        Button(master, text='Select from plate layout', font=("arial", 10, "bold"), command=plate_button, width=25,
+               borderwidth=1).grid(row=40, column=1, pady=10)
+
+        mainloop()
+
+    def plate_gui():
+        slave = Tk()
+
+        platelist = []
+        for i in range(96):
+            platelist.append(BooleanVar())
+
+        for p, i in zip(platelist, range(len(platelist))):
+            if i < 12:
+                Label(slave, font=("arial", 8), width=0, anchor="w", text=i + 1).grid(row=0, column=i + 1)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="A").grid(row=1, column=0)
+                Checkbutton(slave, variable=p).grid(row=1, column=i + 1, padx=0, sticky=W)
+            elif i < 24:
+                Checkbutton(slave, variable=p).grid(row=2, column=i - 11, padx=0, sticky=W)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="B").grid(row=2, column=0)
+            elif i < 36:
+                Checkbutton(slave, variable=p).grid(row=3, column=i - 23, padx=0, sticky=W)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="C").grid(row=3, column=0)
+            elif i < 48:
+                Checkbutton(slave, variable=p).grid(row=4, column=i - 35, padx=0, sticky=W)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="D").grid(row=4, column=0)
+            elif i < 60:
+                Checkbutton(slave, variable=p).grid(row=5, column=i - 47, padx=0, sticky=W)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="E").grid(row=5, column=0)
+            elif i < 72:
+                Checkbutton(slave, variable=p).grid(row=6, column=i - 59, padx=0, sticky=W)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="F").grid(row=6, column=0)
+            elif i < 84:
+                Checkbutton(slave, variable=p).grid(row=7, column=i - 71, padx=0, sticky=W)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="G").grid(row=7, column=0)
+            elif i < 96:
+                Checkbutton(slave, variable=p).grid(row=8, column=i - 83, padx=0, sticky=W)
+                Label(slave, font=("arial", 8), width=0, anchor="w", text="H").grid(row=8, column=0)
+
+        def Submit():
+            slave.destroy()
+
+        Button(slave, text='Submit', font=("arial", 10, "bold"), command=Submit, width=5,
+               borderwidth=1).grid(row=9, column=13, pady=10, padx=10)
 
         mainloop()
 
